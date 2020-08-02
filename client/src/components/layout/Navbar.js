1e6108c8ac3264e97dde007cf37e5a16fe41e155
AchrafHamrit <achraf96@gmail.com>
2020-08-03 00:46:30 +0100

    Added FontAwesome / Navbar Modifications

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { Container, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Navbar, Nav, Button, Dropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+
+// Images
+import Logo from '../../images/logo.svg';
 
 const NavbarComponent = () => {
+  const userMenu = (
+    <>
+      <Dropdown alignRight>
+        <Dropdown.Toggle variant='outline-light'>
+          <FontAwesomeIcon icon={faEllipsisH} size='lg' />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
+          <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
+          <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
+  );
+
+  const guestMenu = (
+    <>
+      <Link to='/login'>
+        <Button variant='outline-light'>Get started</Button>
+      </Link>
+    </>
+  );
+
   return (
     <Navbar bg='white' expand='lg'>
-      <Container className='justify-content-between'>
-        <Navbar.Brand href='#home'>Logo</Navbar.Brand>
-        <Nav className='ml-auto'>
-          <Button variant='outline-light'>Get started</Button>
-        </Nav>
+      <Container>
+        <Link to='/'>
+          <Navbar.Brand>
+            <img src={Logo} alt='tellme' />
+          </Navbar.Brand>
+        </Link>
+
+        <Nav className='ml-auto'>{true ? userMenu : guestMenu}</Nav>
       </Container>
     </Navbar>
   );
