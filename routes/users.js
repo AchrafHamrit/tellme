5e472237380e019aa720f1fc045d5e6c51ca4d21
AchrafHamrit <achraf96@gmail.com>
2020-08-09 01:26:21 +0100

    Added GetUserByUsername route (Backend)

--- routes/users.js
+++ routes/users.js
@@ -103,4 +103,22 @@ router.put('/', auth, async (req, res) => {
   }
 });
 
+//  @route       GET api/users/:username
+//  @desc        Get user by username
+//  @access      Public
+router.get('/:username', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username }).select(
+      '-password -email -date -__v'
+    );
+
+    if (!user) return res.status(404).json([{ msg: 'User not found' }]);
+
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
